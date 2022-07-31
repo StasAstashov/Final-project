715b60781eb575c66b9670229254b9f2758581c8
StasAstashov <109100166+StasAstashov@users.noreply.github.com>
2022-07-31 13:20:23 +0300

    shag 8 proiden razdel 4.2

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,6 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+class LoginPageLocators():
+    Login_Form = (By.ID, "login_form")
+    Register_Form = (By.ID, "register_form")
